a9e8b2fcb9185660541f899d70ba3ae0602a36ad
Kumar Harsh <kumarharsh241998@gmail.com>
2022-05-13 18:08:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hacker Rex</h1>
+     <span> Let's hack Together</span>
+
+    </div>
+  );
+}
+
+export default App;
