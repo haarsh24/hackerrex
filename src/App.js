291be69039bb3dc06bb3df9943c4050c51457ee4
Kumar Harsh <kumarharsh241998@gmail.com>
2022-05-19 16:32:50 +0530

    fixed the warning

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 import { routes } from "./config/routes"
 import { useRoutes } from "react-router-dom"
-import { ScrollToTop, Toast} from "components"
+
 import { Navbar } from "components/navbar/navbar";
 
 function App() {
   const routeElement = useRoutes(routes);
   return (
     <>
-        {/* <ScrollToTop />
-      // <Toast />
-       */}
+       
       <Navbar/>
       <div className="App bg-[#07080B] text-[#9B9B9C]">
         {routeElement} 
