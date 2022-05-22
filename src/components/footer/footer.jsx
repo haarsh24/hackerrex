076f582d21bd4db43c1692c57a161a8d1e260993
Kumar Harsh <kumarharsh241998@gmail.com>
2022-05-22 23:17:22 +0530

    updated footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <>
             <div className=" w-full bg-[#141820]  py-1 lg:px-80 md:px-40  px-20 flex items-center h-16  bottom-0 text-white text-xl ">
-                <span className="">Designed and Developed by <a className="text-sky-500" href="https://www.twitter.com/codeandchords">Harsh</a></span>
+                <span className="">Designed and Developed by <a className="text-sky-500" href="https://kumarharsh.netlify.app/">Harsh</a></span>
             </div>
         </>
     )
